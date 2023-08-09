7da825c92db7a5c8a3ec04ccbe0967232acf6d4d
ThielG <118766124+ThielG@users.noreply.github.com>
2023-08-09 19:46:40 -0300

    Código

--- Cliente.py
+++ Cliente.py
@@ -15,7 +15,7 @@ class Pessoa:
 class Cliente(Pessoa):
     def __init__(self, nome, idade):
         super().__init__(nome, idade)
-        self.conta = None # <- verificar se roda sem esse rolê aqui
+        self.conta = None
     
     def adicionar_conta(self, conta):
         self.conta = conta
